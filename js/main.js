96b135fcc0cbc2dc129302f92d6fd824b651005b
doanvanquoc <0306211505@caothang.edu.vn>
2022-07-06 19:02:48 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let showNav = document.getElementById("showNav");
+let hideNav = document.getElementById("hideNav");
+let mobile_nav = document.getElementById("mobile_nav");
+
+
+showNav.onclick = function () {
+    mobile_nav.style.transform = "translateX(-100%)";
+    mobile_nav.style.transition = "transform 0.3s linear";
+}
+
+hideNav.onclick = function () {
+    mobile_nav.style.transform = "translateX(100%)";
+    mobile_nav.style.transition = "transform 0.3s linear";
+
+}
+
+// 2 function bên dưới là do github copilot làm, e chỉ thêm nó vào để cái navbar thêm hợp lý xíu :((
+// reponsive e chỉ mới làm được trên 740px nhìn nó không bị loạn thôi còn bên mobile thì chưa :((
+
+//create a function to hide the navbar when resize the window
+window.onresize = function () {
+    if (window.innerWidth > 768) {
+        mobile_nav.style.transform = "translateX(100%)";
+        mobile_nav.style.transition = "transform 0.3s linear";
+    }
+}
+
+//create a function to hide the navbar when scroll the page
+window.onscroll = function () {
+    if (window.scrollY > 100) {
+        mobile_nav.style.transform = "translateX(100%)";
+        mobile_nav.style.transition = "transform 0.3s linear";
+    }
+}
+
+
+
