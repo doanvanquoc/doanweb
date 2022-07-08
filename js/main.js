cdaa949a5c4acffe3ddb62c82298648becaeb4de
doanvanquoc <108019767+doanvanquoc@users.noreply.github.com>
2022-07-08 23:07:00 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let showNav = document.getElementById("showNav");
 let hideNav = document.getElementById("hideNav");
 let mobile_nav = document.getElementById("mobile_nav");
+let dropdown_button = document.getElementById("dropdown_button");
+let dropdown_button2 = document.getElementById("dropdown_button2");
+let dropmenu = document.getElementById("dropmenu");
+let dropmenu2 = document.getElementById("dropmenu2");
+let i = 0;
 
 
 showNav.onclick = function () {
@@ -14,6 +19,28 @@ hideNav.onclick = function () {
 
 }
 
+dropdown_button.onclick = function () {
+    if (i % 2 == 0) {
+        dropmenu.style.display = "block";
+        i++;
+    }
+    else {
+        dropmenu.style.display = "none";
+        i++;
+    }
+}
+
+dropdown_button2.onclick = function () {
+    if (i % 2 == 0) {
+        dropmenu2.style.display = "block";
+        i++;
+    }
+    else {
+        dropmenu2.style.display = "none";
+        i++;
+    }
+}
+
 // 2 function bên dưới là do github copilot làm, e chỉ thêm nó vào để cái navbar thêm hợp lý xíu :((
 
 //create a function to hide the navbar when resize the window
@@ -24,13 +51,14 @@ window.onresize = function () {
     }
 }
 
-//create a function to hide the navbar when scroll the page
+// //create a function to hide the navbar when scroll the page
 window.onscroll = function () {
     if (window.scrollY > 100) {
         mobile_nav.style.transform = "translateX(100%)";
         mobile_nav.style.transition = "transform 0.3s linear";
     }
 }
+
 
 
 
